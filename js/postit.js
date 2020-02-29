c7399435427ff9c562d68735a51290d442e16e50
peter-loliveira <peter.loliveira@gmail.com>
2020-02-29 11:24:02 -0300

    Funçaõ EXIBIR postits pronta (melhorar posteriormente)

--- js/postit.js
+++ js/postit.js
@@ -4,23 +4,17 @@ app.style.width = "1400px"
 app.style.margin = "0 auto"
 app.style.marginTop = "20px"
 
-//Cria uma área só para os componente que determinarão as caracteristicas e estilos dos postits
-var divGeral = document.createElement("div")
-divGeral.setAttribute("id", "areaGeral")
-divGeral.setAttribute("class", "areaComponentes")
-divGeral.style.textAlign = "Center"
-app.appendChild(divGeral)
+//Cria uma área só para os componentes que determinarão as caracteristicas e estilos dos postits
+var divComponentes = document.createElement("div")
+divComponentes.setAttribute("id", "areaGeral")
+divComponentes.setAttribute("class", "areaComponentes")
+divComponentes.style.textAlign = "Center"
+app.appendChild(divComponentes)
 
+//Cria uma área DENTRO DA divComponentes só para os componentes irão determinar o texto INTERNO dos Postits
 var divTextoPostit = document.createElement("div")
 divTextoPostit.setAttribute("class", "divs-atributos")
 divTextoPostit.style.alignItems = "center"
-divGeral.appendChild(divTextoPostit)
-
-var divOpacidadePostit = document.createElement("div")
-divOpacidadePostit.setAttribute("class", "divs-atributos")
-divOpacidadePostit.style.alignItems = "center"
-divGeral.appendChild(divOpacidadePostit)
-
 
 var texto = document.createElement("label")
 texto.innerHTML = "Texto interno do Postit: "
@@ -30,26 +24,60 @@ var inpTexto = document.createElement("input")
 inpTexto.placeholder = "Texto do Postit"
 inpTexto.style.textAlign = "center"
 
+divComponentes.appendChild(divTextoPostit)
 
-
+//Cria uma área DENTRO DA divComponentes só para os componentes irão determinar a OPACIDADE dos Postits
+var divOpacidadePostit = document.createElement("div")
+divOpacidadePostit.setAttribute("class", "divs-atributos")
+divOpacidadePostit.style.alignItems = "center"
+//Label
 var lbOpacidade = document.createElement("label")
 lbOpacidade.innerHTML = "Opacidade do Postit: "
 lbOpacidade.style.fontWeight = "bold"
-
+//Input tipo range
 var inpOpacidade = document.createElement("input")
 inpOpacidade.setAttribute("type", "range")
 inpOpacidade.setAttribute("min", 30)
 inpOpacidade.setAttribute("max", 100)
 inpOpacidade.style.marginRight = "20px"
 inpOpacidade.value = "30"
-
+inpOpacidade.onclick = function () {
+    lbOpacidadeEscolhida.innerHTML = `${100 - inpOpacidade.value}%`
+}
+//label mostrando em quantos % o postit deverá aparecer
 var lbOpacidadeEscolhida = document.createElement("label")
 lbOpacidadeEscolhida.innerHTML = "70%"
 lbOpacidadeEscolhida.style.fontWeight = "bold"
 
-inpOpacidade.onclick = function (params) {
-    lbOpacidadeEscolhida.innerHTML = `${100 - inpOpacidade.value}%`
+//insere a div de opacidade e totos os seus componentes na div de componentes
+divComponentes.appendChild(divOpacidadePostit)
+
+//Cria uma área só para os componentes que farão com que a div dos postits apareca ou suma
+var divVisibilidadePostits = document.createElement("div")
+divVisibilidadePostits.setAttribute("class", "areaComponentes")
+divVisibilidadePostits.style.alignItems = "center"
+divComponentes.appendChild(divVisibilidadePostits)
+
+// Inserie dois elemntos (label + checkbox) para fazer com que os postits aparecam ou desapareçam
+var lbVisualizarPostits = document.createElement("label")
+lbVisualizarPostits.innerHTML = "Ver Postits"
+lbVisualizarPostits.style.fontWeight = "bold"
+var cbVisualizarPostits = document.createElement("input")
+cbVisualizarPostits.setAttribute("type", "checkbox")
+cbVisualizarPostits.checked = true
+cbVisualizarPostits.onclick = function () {
+    let exibePostits = document.getElementsByClassName("postit")
+    if (cbVisualizarPostits.checked){
+        areaPostits.style.display = "block"
+        // exibePostits.style.display = "block"
+    } else{
+        areaPostits.style.display = "none"
+        // exibePostits.style.display = "none"
+    }
+    
 }
+// cbVisualizarPostits.setAttribute("checked", true)
+
 
 //adiciona os componentes dessa área
 var btAddPostit = document.createElement("button")
@@ -64,13 +92,16 @@ var br = document.createElement("br")
 
 divTextoPostit.appendChild(texto)
 divTextoPostit.appendChild(inpTexto)
-divGeral.appendChild(document.createElement("br"))
+divComponentes.appendChild(document.createElement("br"))
 divOpacidadePostit.appendChild(lbOpacidade)
 divOpacidadePostit.appendChild(inpOpacidade)
 divOpacidadePostit.appendChild(lbOpacidadeEscolhida)
-divGeral.appendChild(document.createElement("br"))
+divComponentes.appendChild(document.createElement("br"))
 
-divGeral.appendChild(btAddPostit)
+divComponentes.appendChild(btAddPostit)
+divComponentes.appendChild(divVisibilidadePostits)
+divVisibilidadePostits.appendChild(cbVisualizarPostits)
+divVisibilidadePostits.appendChild(lbVisualizarPostits)
 
 var areaPostits = document.createElement("div")
 areaPostits.setAttribute("class", "areaComponentes")
