d1de99b4bfa6cb82caf0153373443c9994c59d6d
peter-loliveira <peter.loliveira@gmail.com>
2020-02-28 23:10:18 -0300

    Primeiras Funcionalidade do Botão OK

--- js/postit.js
+++ js/postit.js
@@ -3,6 +3,7 @@ var app = document.getElementById("Postit")
 
 var divGeral = document.createElement("div")
 divGeral.setAttribute("id", "areaGeral")
+divGeral.style.textAlign = "Center"
 app.appendChild(divGeral)
 
 var btAddPostit = document.createElement("button")
@@ -13,15 +14,31 @@ inpTexto.setAttribute("id", "inputTexto")
 
 var pTexto = document.createElement("p")
 pTexto.innerHTML = "Vamos lá"
-divGeral.appendChild(btAddPostit)
 divGeral.appendChild(inpTexto)
+divGeral.appendChild(btAddPostit)
 divGeral.appendChild(pTexto)
-var textoPostit = createElement("p")
-textoPostit.innerHTML = document.getElementById("inputTexto").value
 
-function inserirPostit(){
-var postit = document.createElement("div")
-postit.setAttribute("class", "postit")
+function inserirPostit() {
+    let postit = document.createElement("div")
+    postit.setAttribute("class", "postit")
+    let textoPostit = document.createElement("p")
+    textoPostit.innerHTML = inpTexto.value
+    postit.style.width = "200px"
+    postit.style.height = "200px"
+    postit.style.borderRadius = "5px"
+    postit.style.textAlign = "center"
+    postit.appendChild(textoPostit)
+    divGeral.appendChild(postit)
+    postit.style.backgroundColor = gera_cor()
+}
 
-divGeral.appendChild(postit)
+function gera_cor(){
+    var hexadecimais = '0123456789ABCDEF';
+    var cor = '#';  
+    // Pega um número aleatório no array acima
+    for (var i = 0; i < 6; i++ ) {
+    //E concatena à variável cor
+        cor += hexadecimais[Math.floor(Math.random() * 16)];
+    }
+    return cor;
 }
