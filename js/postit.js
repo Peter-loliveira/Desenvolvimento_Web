ef5a8765f43c179b477911859f68e25bdefe70ed
peter-loliveira <peter.loliveira@gmail.com>
2020-03-08 23:27:03 -0300

    Verificar Erro Funcção Danceteria

--- js/postit.js
+++ js/postit.js
@@ -1,206 +1,233 @@
-    var app = document.getElementById("Postit")
-    app.style.textAlign = "center"
-    app.style.width = "1000px"
-    app.style.margin = "0 auto"
-    app.style.marginTop = "20px"
-    
+var app = document.getElementById("Postit")
+app.style.textAlign = "center"
+app.style.width = "1000px"
+app.style.margin = "0 auto"
+app.style.marginTop = "20px"
 
-    //Cria uma área só para os componentes que determinarão as caracteristicas e estilos dos postits
-    var divComponentes = document.createElement("div")
-    // divComponentes.setAttribute("id", "areaGeral")
-    divComponentes.setAttribute("class", "areaComponentes")
-    divComponentes.style.textAlign = "Center"
-        
-    app.appendChild(divComponentes)
 
-    var imgPostit = document.createElement("img")
-    imgPostit.setAttribute("src", "img/postit.png")
-    imgPostit.style.width = "300px"
-    
-    // imgPostit.style.border = "1px solid black"
-    // imgPostit.style.borderRadius = "10%"
-    imgPostit.style.opacity = "70%"
+//Cria uma área só para os componentes que determinarão as caracteristicas e estilos dos postits
+var divComponentes = document.createElement("div")
+// divComponentes.setAttribute("id", "areaGeral")
+divComponentes.setAttribute("class", "areaComponentes")
+divComponentes.style.textAlign = "Center"
 
-    divComponentes.appendChild(imgPostit)
+app.appendChild(divComponentes)
 
-    //Cria uma área DENTRO DA divComponentes só para os componentes irão determinar o texto INTERNO dos Postits
-    var divTextoPostit = document.createElement("div")
-    divTextoPostit.setAttribute("class", "divs-atributos")
-    divTextoPostit.style.alignItems = "center"
+var imgPostit = document.createElement("img")
+imgPostit.setAttribute("src", "img/postit.png")
+imgPostit.style.width = "300px"
 
-    var texto = document.createElement("label")
-    texto.innerHTML = "Texto interno do Postit: "
-    texto.style.fontWeight = "bold"
+// imgPostit.style.border = "1px solid black"
+// imgPostit.style.borderRadius = "10%"
+imgPostit.style.opacity = "70%"
 
-    var inpTexto = document.createElement("input")
-    inpTexto.placeholder = "Texto do Postit"
-    inpTexto.style.textAlign = "center"
-    inpTexto.style.borderRadius = "5px"
+divComponentes.appendChild(imgPostit)
 
-    divComponentes.appendChild(divTextoPostit)
+//Cria uma área DENTRO DA divComponentes só para os componentes irão determinar o texto INTERNO dos Postits
+var divTextoPostit = document.createElement("div")
+divTextoPostit.setAttribute("class", "divs-atributos")
+divTextoPostit.style.alignItems = "center"
 
-    //Cria uma área DENTRO DA divComponentes só para os componentes irão determinar a OPACIDADE dos Postits
-    var divOpacidadePostit = document.createElement("div")
-    divOpacidadePostit.setAttribute("class", "divs-atributos")
-    divOpacidadePostit.style.alignItems = "center"
-    //Label
-    var lbOpacidade = document.createElement("label")
-    lbOpacidade.innerHTML = "Opacidade(max 70%): "
-    lbOpacidade.style.fontWeight = "bold"
-    //Input tipo range
-    var inpOpacidade = document.createElement("input")
-    inpOpacidade.setAttribute("type", "range")
-    inpOpacidade.setAttribute("min", 0)
-    inpOpacidade.setAttribute("max", 70)
-    inpOpacidade.style.marginRight = "20px"
-    inpOpacidade.value = "0"//Deixa a barra de RANGE selecionada no inicio
+var texto = document.createElement("label")
+texto.innerHTML = "Texto interno do Postit: "
+texto.style.fontWeight = "bold"
 
-    //Evento disparado quando mudamos o valor da opacidade POR MOUSE
-    inpOpacidade.onclick = function () {
-        opacidade()
+var inpTexto = document.createElement("input")
+inpTexto.placeholder = "Texto do Postit"
+inpTexto.style.textAlign = "center"
+inpTexto.style.borderRadius = "5px"
+
+divComponentes.appendChild(divTextoPostit)
+
+//Cria uma área DENTRO DA divComponentes só para os componentes irão determinar a OPACIDADE dos Postits
+var divOpacidadePostit = document.createElement("div")
+divOpacidadePostit.setAttribute("class", "divs-atributos")
+divOpacidadePostit.style.alignItems = "center"
+//Label
+var lbOpacidade = document.createElement("label")
+lbOpacidade.innerHTML = "Opacidade(max 70%): "
+lbOpacidade.style.fontWeight = "bold"
+//Input tipo range
+var inpOpacidade = document.createElement("input")
+inpOpacidade.setAttribute("type", "range")
+inpOpacidade.setAttribute("min", 0)
+inpOpacidade.setAttribute("max", 70)
+inpOpacidade.style.marginRight = "20px"
+inpOpacidade.value = "0"//Deixa a barra de RANGE selecionada no inicio
+
+//Evento disparado quando mudamos o valor da opacidade POR MOUSE
+inpOpacidade.onclick = function () {
+    opacidade()
+}
+//Evento disparado quando mudamos o valor da opacidade POR MOVIMENTO DO MOUSE
+inpOpacidade.onmousemove = function () {
+    opacidade()
+}
+//Evento disparado quando mudamos o valor da opacidade POR TECLADO
+inpOpacidade.onkeydown = function () {
+    opacidade()
+}
+var totalOpacidade = inpOpacidade.value//Guarda o valor da opacidade
+
+//label mostrando em quantos % de opacidade o postit deverá aparecer
+var lbOpacidadeEscolhida = document.createElement("label")
+lbOpacidadeEscolhida.innerHTML = "0%"
+lbOpacidadeEscolhida.style.fontWeight = "bold"
+
+//insere a div de opacidade e totos os seus componentes na div de componentes
+divComponentes.appendChild(divOpacidadePostit)
+
+//Cria uma área só para os componentes que farão com que a div dos postits apareca ou suma
+var divVisibilidadePostits = document.createElement("div")
+divVisibilidadePostits.setAttribute("class", "areaComponentes")
+divVisibilidadePostits.style.alignItems = "center"
+divComponentes.appendChild(divVisibilidadePostits)
+
+// Inserie dois elemntos (label + checkbox) para fazer com que os postits aparecam ou desapareçam
+var lbVisualizarPostits = document.createElement("label")
+lbVisualizarPostits.innerHTML = "Ver Postits"
+lbVisualizarPostits.style.fontWeight = "bold"
+
+var cbVisualizarPostits = document.createElement("input")
+cbVisualizarPostits.setAttribute("type", "checkbox")
+cbVisualizarPostits.checked = true
+cbVisualizarPostits.onclick = function () {
+    morreDiabo()
+}
+lbVisualizarPostits.appendChild(cbVisualizarPostits)
+
+var btAddPostit = document.createElement("button")
+btAddPostit.innerHTML = "Adicione Postit"
+btAddPostit.style.fontWeight = "bold"
+btAddPostit.style.marginTop = "5px"
+btAddPostit.onclick = function () {
+    inserirPostit()//Chama a função de inserir postits
+}
+
+//adiciona os componentes dessa área
+divTextoPostit.appendChild(texto)
+divTextoPostit.appendChild(inpTexto)
+divComponentes.appendChild(document.createElement("br"))
+
+divOpacidadePostit.appendChild(lbOpacidade)
+divOpacidadePostit.appendChild(inpOpacidade)
+divOpacidadePostit.appendChild(lbOpacidadeEscolhida)
+divComponentes.appendChild(document.createElement("br"))
+
+divComponentes.appendChild(btAddPostit)
+divComponentes.appendChild(btAddPostit)
+divComponentes.appendChild(document.createElement("br"))
+divComponentes.appendChild(document.createElement("br"))
+
+divComponentes.appendChild(divVisibilidadePostits)
+divVisibilidadePostits.appendChild(lbVisualizarPostits)
+
+var areaPostits = document.createElement("div")
+areaPostits.setAttribute("class", "areaComponentes")
+
+var textoAreaPostis = document.createElement("h1")
+textoAreaPostis.innerHTML = "Postits Criados"
+areaPostits.appendChild(textoAreaPostis)
+
+var areaDanceteria = document.createElement("div")
+// var lbDanceteria = document.createElement("label")
+// lbDanceteria.innerHTML = "Ligar Danceteria"
+// lbDanceteria.setAttribute("class", "lbDanceteria")
+var lbVoltasDanceteria = document.createElement("label")
+lbVoltasDanceteria.innerHTML = "Quantas voltas?"
+lbVoltasDanceteria.setAttribute("class", "lbDanceteria")
+
+
+var inpVoltasDanceteria = document.createElement("input")
+inpVoltasDanceteria.setAttribute("type", "number")
+inpVoltasDanceteria.setAttribute("min", "10")
+inpVoltasDanceteria.setAttribute("mam", "500")
+inpVoltasDanceteria.value = "10"
+inpVoltasDanceteria.style.width = "70px"
+
+var btDanceteria = document.createElement("button")
+btDanceteria.innerHTML = "LIGAR"
+btDanceteria.onclick = function () {
+    ligarDanceteria(inpVoltasDanceteria.value)
+}
+areaDanceteria.appendChild(inpVoltasDanceteria)
+areaDanceteria.appendChild(btDanceteria)
+
+areaPostits.appendChild(areaDanceteria)
+app.appendChild(areaPostits)
+
+function inserirPostit() {
+    let postit = document.createElement("button")
+    postit.innerHTML = inpTexto.value
+    postit.setAttribute("class", "postit")
+    postit.style.width = "200px"
+    postit.style.height = "200px"
+    postit.style.borderRadius = "5px"
+    postit.style.border = "1px solid black"
+    postit.style.marginBottom = "5px"
+    postit.style.marginTop = "5px"
+    postit.style.marginLeft = "5px"
+    postit.style.fontSize = "15px"
+    postit.style.fontWeight = "bold"
+    postit.style.overflow = "auto" //Faz com que, SE o texto interno do Postit for maior que ele, apareçam barras de rolagem internas
+    postit.style.opacity = `${100 - totalOpacidade}%`
+
+    postit.ondblclick = function () {
+        postit.style.display = "none"
     }
-    //Evento disparado quando mudamos o valor da opacidade POR MOVIMENTO DO MOUSE
-    inpOpacidade.onmousemove = function () {
-        opacidade()
-    }
-    //Evento disparado quando mudamos o valor da opacidade POR TECLADO
-    inpOpacidade.onkeydown = function () {
-        opacidade()
-    }
-    var totalOpacidade = inpOpacidade.value//Guarda o valor da opacidade
 
-    //label mostrando em quantos % de opacidade o postit deverá aparecer
-    var lbOpacidadeEscolhida = document.createElement("label")
-    lbOpacidadeEscolhida.innerHTML = "0%"
-    lbOpacidadeEscolhida.style.fontWeight = "bold"
+    postit.style.backgroundColor = gera_cor()
 
-    //insere a div de opacidade e totos os seus componentes na div de componentes
-    divComponentes.appendChild(divOpacidadePostit)
-
-    //Cria uma área só para os componentes que farão com que a div dos postits apareca ou suma
-    var divVisibilidadePostits = document.createElement("div")
-    divVisibilidadePostits.setAttribute("class", "areaComponentes")
-    divVisibilidadePostits.style.alignItems = "center"
-    divComponentes.appendChild(divVisibilidadePostits)
-
-    // Inserie dois elemntos (label + checkbox) para fazer com que os postits aparecam ou desapareçam
-    var lbVisualizarPostits = document.createElement("label")
-    lbVisualizarPostits.innerHTML = "Ver Postits"
-    lbVisualizarPostits.style.fontWeight = "bold"
-
-    var cbVisualizarPostits = document.createElement("input")
-    cbVisualizarPostits.setAttribute("type", "checkbox")
-    cbVisualizarPostits.checked = true
-    cbVisualizarPostits.onclick = function () {
-        morreDiabo()
-    }
-    lbVisualizarPostits.appendChild(cbVisualizarPostits)
-
-    var btAddPostit = document.createElement("button")
-    btAddPostit.innerHTML = "Adicione Postit"
-    btAddPostit.style.fontWeight = "bold"
-    btAddPostit.style.marginTop = "5px"
-    btAddPostit.onclick = function () {
-        inserirPostit()//Chama a função de inserir postits
-    }
-
-    //adiciona os componentes dessa área
-    divTextoPostit.appendChild(texto)
-    divTextoPostit.appendChild(inpTexto)
-    divComponentes.appendChild(document.createElement("br"))
-
-    divOpacidadePostit.appendChild(lbOpacidade)
-    divOpacidadePostit.appendChild(inpOpacidade)
-    divOpacidadePostit.appendChild(lbOpacidadeEscolhida)
-    divComponentes.appendChild(document.createElement("br"))
-
-    divComponentes.appendChild(btAddPostit)
-    divComponentes.appendChild(btAddPostit)
-    divComponentes.appendChild(document.createElement("br"))
-    divComponentes.appendChild(document.createElement("br"))
-
-    divComponentes.appendChild(divVisibilidadePostits)
-    // divVisibilidadePostits.appendChild(cbVisualizarPostits)
-    divVisibilidadePostits.appendChild(lbVisualizarPostits)
-
-    var areaPostits = document.createElement("div")
-    areaPostits.setAttribute("class", "areaComponentes")
-
-    var textoAreaPostis = document.createElement("h1")
-    textoAreaPostis.innerHTML = "Postits Criados"
-    areaPostits.appendChild(textoAreaPostis)
-
-    app.appendChild(areaPostits)
-
-    function inserirPostit() {
-        let postit = document.createElement("button")
-        postit.innerHTML = inpTexto.value
-        postit.setAttribute("class", "postit")
-        postit.style.width = "200px"
-        postit.style.height = "200px"
-        postit.style.borderRadius = "5px"
-        postit.style.border = "1px solid black"
-        postit.style.marginBottom = "5px"
-        postit.style.marginTop = "5px"
-        postit.style.marginLeft = "5px"
-        postit.style.fontSize = "15px"
-        postit.style.fontWeight = "bold"
-        postit.style.overflow = "auto" //Faz com que, SE o texto interno do Postit for maior que ele, apareçam barras de rolagem internas
-        postit.style.opacity = `${100 - totalOpacidade}%`
-        
-        postit.ondblclick = function () {
-            postit.style.display = "none"
-        }
-        
+    postit.onclick = function () {
         postit.style.backgroundColor = gera_cor()
-        
-        postit.onclick = function () {
-            postit.style.backgroundColor = gera_cor()
-        }
-
-        if(!cbVisualizarPostits.checked){
-            postit.style.display = "none"
-        }
-
-        let textOpacidade = document.createElement("p")
-        textOpacidade.innerHTML = `Opacidade de ${totalOpacidade}%`
-        textOpacidade.style.fontWeight = "bold"
-        postit.appendChild(textOpacidade)
-        areaPostits.appendChild(postit)
     }
 
-    function gera_cor() {
-        var hexadecimais = '0123456789ABCDEF';
-        var cor = '#';
-        // Pega um número aleatório no array acima
-        for (var i = 0; i < 6; i++) {
-            //E concatena à variável cor
-            cor += hexadecimais[Math.floor(Math.random() * 16)];
-        }
-        return cor;
+    if (!cbVisualizarPostits.checked) {
+        postit.style.display = "none"
     }
 
-    function opacidade() {
-        totalOpacidade = inpOpacidade.value
-        lbOpacidadeEscolhida.innerHTML = `${totalOpacidade}%`
-    }
+    let textOpacidade = document.createElement("p")
+    textOpacidade.innerHTML = `Opacidade de ${totalOpacidade}%`
+    textOpacidade.style.fontWeight = "bold"
+    postit.appendChild(textOpacidade)
+    areaPostits.appendChild(postit)
+}
 
-    function morreDiabo() {
-        var totalPostits = document.getElementsByClassName("postit")
-        for (i = 0; i < totalPostits.length; i++) {
-            if (cbVisualizarPostits.checked) {
-                totalPostits[i].style.display = "inline-flex"
-            } else {
-                totalPostits[i].style.display = "none"
-            }
+function gera_cor() {
+    var hexadecimais = '0123456789ABCDEF';
+    var cor = '#';
+    // Pega um número aleatório no array acima
+    for (var i = 0; i < 6; i++) {
+        //E concatena à variável cor
+        cor += hexadecimais[Math.floor(Math.random() * 16)];
+    }
+    return cor;
+}
+
+function opacidade() {
+    totalOpacidade = inpOpacidade.value
+    lbOpacidadeEscolhida.innerHTML = `${totalOpacidade}%`
+}
+
+function morreDiabo() {
+    let totalPostits = document.getElementsByClassName("postit")
+    for (i = 0; i < totalPostits.length; i++) {
+        if (cbVisualizarPostits.checked) {
+            totalPostits[i].style.display = "inline-flex"
+        } else {
+            totalPostits[i].style.display = "none"
         }
     }
+}
 
-    switch (key) {
-        case value:
-            
-            break;
-    
-        default:
-            break;
+function ligarDanceteria(voltas) {
+    let totalPostitsVoltas = document.getElementsByClassName("postit")
+    for (a = 1; a < voltas; a++) {
+        for (b = 0; b <= totalPostitsVoltas.length; b++) {
+            let cor = gera_cor()
+            totalPostitsVoltas[b].style.backgroundColor = cor
+            // alert(`Volta ${a} - ${b} - ${cor}`)
+            // alert(totalPostitsVoltas)
+        }
     }
+}
